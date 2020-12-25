bad41755299c35def721c8b8404ae4b8760db15d
Alex Whitler <alex.whitler@gmail.com>
2020-12-24 22:04:57 -0500

    fish and asdf updates

--- macos/roles/terminal/files/fish/config.fish
+++ macos/roles/terminal/files/fish/config.fish
@@ -1,2 +1,7 @@
-source ~/.asdf/asdf.fish
+set -g fish_user_paths "/usr/local/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set PATH $PATH /usr/local/opt/asdf/bin
+set PATH $PATH /Users/alexwhitler/.cargo/bin
+set PATH $PATH $HOME/.asdf/shims
+set PATH (asdf where golang)/packages/bin $PATH
 starship init fish | source
